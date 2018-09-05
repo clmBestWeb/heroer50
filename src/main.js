212aefc8c605dac25c161dd427db68d61830f3ec
clm <601974060@qq.com>
2018-09-05 16:32:19 +0800

    路由组件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 
 // 导入全局的样式
 // 导入bootstrap样式
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css';
+
+// 导入路由组件
+import HeroList from './views/heroes/List.vue'
+import EquipList from './views/equips/List.vue'
+import WeaponList from './views/weapons/List.vue'
+
+//Vue.use()注册VueRouter插件
+Vue.use(VueRouter);
+
+//创建路由对象
+const router = new VueRouter({
+    // 路由规则
+    routes: [
+        { path: '/heroes', component: HeroList },
+        { path: '/equips', component: EquipList },
+        { path: '/weapons', component: WeaponList }
+    ]
+})
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    router
 })
