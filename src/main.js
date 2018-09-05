07c0a297ac993d2f326690041d0e5d93e8ef0922
clm <601974060@qq.com>
2018-09-05 14:57:11 +0800

    app-sidebar

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 // 导入全局的样式
 // 导入bootstrap样式
 import 'bootstrap/dist/css/bootstrap.css';
-import './assets/css/index.css'
+import './assets/css/index.css';
 new Vue({
     el: '#app',
     render: h => h(App)
