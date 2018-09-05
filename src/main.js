ba0471caa13c738603a8bbabb484a4004fcca3a6
clm <601974060@qq.com>
2018-09-05 11:35:57 +0800

    配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
+// 导入全局的样式
+// 导入bootstrap样式
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css'
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
